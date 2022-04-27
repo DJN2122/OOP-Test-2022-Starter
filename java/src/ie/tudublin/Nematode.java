ba75fc21779f36246db0816b02c99baefb495780
DJN2122 <david_niculita@yahoo.com>
2022-04-27 17:54:15 +0100

    constructor

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,64 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Nematode {
+    
+    private String name;
+    private float length;
+    private boolean limbs; 
+    private char gender;
+    private boolean eyes;
+
+    public Nematode(TableRow tr)
+    {
+        this(
+            tr.getString("name"), 
+            tr.getFloat("length"),
+            tr.getInt("limbs") == 1,
+            tr.get("gender"),
+            tr.getInt("eyes") == 1,
+        );
+    }
+
+    public Nematode(String name, float length, boolean limbs, char gender, boolean eyes) {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public float getLength() {
+        return length;
+    }
+    public void setLength(float length) {
+        this.length = length;
+    }
+    public boolean getLimbs() {
+        return limbs;
+    }
+    public void setLimbs(boolean limbs) {
+        this.limbs = limbs;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public boolean getEyes() {
+        return eyes;
+    }
+    public void setEyes(boolean eyes) {
+        this.eyes = eyes;
+    } 
+
+    
+
+}
