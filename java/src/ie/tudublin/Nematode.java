4f85f279b08d86cfbe9df3cf5f8bda5ef8f69bc1
DJN2122 <david_niculita@yahoo.com>
2022-04-27 18:49:14 +0100

    changed constructor, imported necessary files, implemented array list and called csv file. Also cleaned up code and added comments

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -1,64 +1,92 @@
+/*
+@Author: David Niculita
+@Student No: C20513979
+A Java Program to attempt to create and number of different metanodes using processing. 
+*/
+
 package ie.tudublin;
 
+import processing.core.PApplet; //import files to generate the nematodes.
 import processing.data.TableRow;
 
-public class Nematode {
+
+//Create new nematode class
+public class Nematode extends PApplet {
+    // create fields
+	private String name;
+	private int length;
+	private boolean limbs;
+	private String gender;
+	private boolean eyes;
     
-    private String name;
-    private float length;
-    private boolean limbs; 
-    private char gender;
-    private boolean eyes;
-
-    public Nematode(TableRow tr)
-    {
-        this(
-            tr.getString("name"), 
-            tr.getFloat("length"),
-            tr.getInt("limbs") == 1,
-            tr.get("gender"),
-            tr.getInt("eyes") == 1,
-        );
+    // toString function for each field genetrated with source action
+    @Override
+    public String toString() {
+        return "Nematode [eyes=" + eyes + ", gender=" + gender + ", length=" + length + ", limbs=" + limbs
+                + ", name=" + name + "]";
     }
-
-    public Nematode(String name, float length, boolean limbs, char gender, boolean eyes) {
-        this.name = name;
-        this.length = length;
-        this.limbs = limbs;
-        this.gender = gender;
-        this.eyes = eyes;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public float getLength() {
-        return length;
-    }
-    public void setLength(float length) {
-        this.length = length;
-    }
-    public boolean getLimbs() {
-        return limbs;
-    }
-    public void setLimbs(boolean limbs) {
-        this.limbs = limbs;
-    }
-    public char getGender() {
-        return gender;
-    }
-    public void setGender(char gender) {
-        this.gender = gender;
-    }
-    public boolean getEyes() {
-        return eyes;
-    }
-    public void setEyes(boolean eyes) {
-        this.eyes = eyes;
-    } 
-
     
+	public Nematode(TableRow tr) // get table row information
+	{
+		this(
+			tr.getString("name"), 
+			tr.getInt("length"), 
+			tr.getInt("limbs") == 1, 
+			tr.getString("gender"), 
+			tr.getInt("eyes") == 1)
+        ;
+	}
+
+	public Nematode(String name, int length, boolean limbs, String gender, boolean eyes) { // constructor for the fields created 
+		this.name = name;
+		this.length = length;
+		this.limbs = limbs;
+		this.gender = gender;
+		this.eyes = eyes;
+	}
+
+
+	// getters and setters automatically generated with source action
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public boolean isLimbs() {
+		return limbs;
+	}
+	public void setLimbs(boolean limbs) {
+		this.limbs = limbs;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public boolean isEyes() {
+		return eyes;
+	}
+	public void setEyes(boolean eyes) {
+		this.eyes = eyes;
+	}
+
+	public void render(Nematode pa)
+	{
+		float x = map(length, 0, 255, 0, 800);
+		float y = map(length, 0, 255, 800, 0);
+
+		text(name, 400, 200);
+		circle(400, 400, length);
+		stroke(x, y, 0);
+		noFill();
+	}
 
 }
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -1,3 +1,9 @@
+/*
+@Author: David Niculita
+@Student No: C20513979
+A Java Program to attempt to create and number of different metanodes using processing.
+*/
+
 package ie.tudublin;
 
 import java.util.ArrayList;
@@ -5,18 +11,62 @@ import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
-
+	
 public class NematodeVisualiser extends PApplet
 {
+	float height = 800;
+	float width = 800;
+
+	ArrayList<Nematode> nematodes = new ArrayList<Nematode>(); // array list for the nematodes
 
 	public void keyPressed()
 	{		
 		if (keyCode == LEFT)
 		{
-		}		
+
+
+		}	
+		
+		if (keyCode == RIGHT)
+		{
+			
+
+		}
 	}
+	
+	public void draw()
+	{	
+		stroke(255, 0, 0);
+		strokeWeight(3);
 
+		// left arrow
+		line(width - 650, height / 2 , width - 550, height / 2);
+		line(width - 650, height / 2, width - 630, height - 420);
+		line(width - 650, height / 2, width - 630, height - 380);
 
+		// right arrow
+		line(width - 150, height / 2, width - 250, height / 2);
+		line(width - 150, height / 2, width - 170, height - 380);
+		line(width - 150, height / 2, width - 170, height - 420);
+
+		for(Nematode n:nematodes)
+		{
+			n.render(this);
+		}
+
+	}
+	
+	public void loadNematodes()
+	{
+		Table table = loadTable("nematodes.csv", "header"); // calling loadTable function to read in csv file
+
+		for(TableRow r:table.rows())
+		{
+			Nematode n = new Nematode(r);
+			nematodes.add(n);
+		}
+	}
+	
 	public void settings()
 	{
 		size(800, 800);
@@ -24,18 +74,10 @@ public class NematodeVisualiser extends PApplet
 
 	public void setup() 
 	{
-		colorMode(HSB);
+		colorMode(RGB);
 		background(0);
-		smooth();				
-	}
-	
-
-	public void loadNematodes()
-	{
+		smooth();	
+		loadNematodes();		
 	}
 
-
-	public void draw()
-	{	
-	}
 }
